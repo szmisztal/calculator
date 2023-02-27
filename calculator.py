85950a0049fc72ce321ea83ddd567a2de04973ee
Szymon Misztal <sz.misztal@gmail.com>
2023-02-27 12:39:26 +0100

    add math operations

--- calculator.py
+++ calculator.py
@@ -1,27 +1,33 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-def addition(x, y):
-    return x + y
-
-def subtraction(x, y):
-    return x - y
-
-def multiplication(x, y):
-    return x * y
-
-def division (x, y):
-    return x / y
-
-math_operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-if math_operation == 1:
-    addition
-elif math_operation == 2:
-    subtraction
-elif math_operation == 3:
-    multiplication
-elif math_operation == 4:
-    division
+math_operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 - Dodawanie, 2 - Odejmowanie, 3 - Mnożenie, 4 - Dzielenie: ")
 
 num_x = float(input("Podaj pierwszą liczbę: "))
 num_y = float(input("Podaj drugą liczbę: "))
+
+def addition(x, y):
+    logging.info(f"Dodaję {num_x} i {num_y}")
+    return x + y
+
+def subtraction(x, y):
+    logging.info(f"Odejmuję {num_y} od {num_x}")
+    return x - y
+
+def multiplication(x, y):
+    logging.info(f"Mnożę {num_x} przez {num_y}")
+    return x * y
+
+def division (x, y):
+    logging.info(f"Dzielę {num_x} przez {num_y}")
+    return x / y
+
+if math_operation == '1':
+    print("Wynik to: ", addition(num_x, num_y))
+elif math_operation == '2':
+    print("Wynik to: ", subtraction(num_x, num_y))
+elif math_operation == '3':
+    print("Wynik to: ", multiplication(num_x, num_y))
+elif math_operation == '4':
+    print ("Wynik to: ", division(num_x, num_y))
+
