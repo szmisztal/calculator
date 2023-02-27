24132035532dcfa2b321cdf506fbc40fc647b343
Szymon Misztal <sz.misztal@gmail.com>
2023-02-27 15:14:47 +0100

    changes

--- calculator.py
+++ calculator.py
@@ -1,11 +1,6 @@
+import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
-
-math_operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 - Dodawanie, 2 - Odejmowanie, 3 - Mnożenie, 4 - Dzielenie: ")
-
-num_x = float(input("Podaj pierwszą liczbę: "))
-num_y = float(input("Podaj drugą liczbę: "))
-
 
 def addition(x, y):
     logging.info(f"Dodaję {num_x} i {num_y}")
@@ -23,17 +18,30 @@ def division (x, y):
     logging.info(f"Dzielę {num_x} przez {num_y}")
     return x / y
 
+math_operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 - Dodawanie, 2 - Odejmowanie, 3 - Mnożenie, 4 - Dzielenie: ")
+
 if math_operation == '1':
+    num_x = float(input("Podaj pierwszą liczbę: "))
+    num_y = float(input("Podaj drugą liczbę: "))
     print("Wynik to: ", addition(num_x, num_y), )
 
 elif math_operation == '2':
+    num_x = float(input("Podaj pierwszą liczbę: "))
+    num_y = float(input("Podaj drugą liczbę: "))
     print("Wynik to: ", subtraction(num_x, num_y))
 
 elif math_operation == '3':
+    num_x = float(input("Podaj pierwszą liczbę: "))
+    num_y = float(input("Podaj drugą liczbę: "))
     print("Wynik to: ", multiplication(num_x, num_y))
 
 elif math_operation == '4':
+    num_x = float(input("Podaj pierwszą liczbę: "))
+    num_y = float(input("Podaj drugą liczbę: "))
     print ("Wynik to: ", division(num_x, num_y))
 
+else:
+    print("Niepoprawna wartość.")
+    
 
 
